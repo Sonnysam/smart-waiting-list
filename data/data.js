b08141817df7d511f7b2efe55b9eb736dbae5a37
sonnysam <samuelagbenyo067@gmail.com>
2023-04-24 13:14:40 +0000

    data file added

---
+++ data/data.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    id: "1",
+    name: "Samuel Agbenyo",
+    image: require("../assets/images/user.png"),
+  },
+    {
+    id: "2",
+    name: "John Teye",
+    image: require("../assets/images/user.png"),
+    },
+    {
+    id: "3",
+    name: "Bismark Yamoah",
+    image: require("../assets/images/user.png"),
+    },
+];
